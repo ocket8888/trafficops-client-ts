28f108fe35c60f10686a01aa7e57ce3f446b3304
ocket8888 <ocket8888@apache.org>
2022-05-25 08:30:51 -0600

    Handle errors from main function

--- main.ts
+++ main.ts
@@ -1,8 +1,19 @@
 #!/usr/bin/env node
-import { errors } from "trafficops-types";
+import { type Alert, errors } from "trafficops-types";
 
 import { Client } from "./index.js";
 
+/**
+ * Small convenience function that checks if a response object has any error
+ * Alerts, and returns a value that can be added to the return code to
+ * effectively count errors encountered.
+ *
+ * @param chkMe The object containing the Alerts to check.
+ * @returns `1` if there were errors in the `chkMe` Alerts, `0` otherwise.
+ */
+function checkAlerts(chkMe?: {alerts?: Array<Alert>} | null | undefined): 1 | 0 {
+	return chkMe && chkMe.alerts && chkMe.alerts.length > 0 && errors(chkMe.alerts).length > 0 ? 1 : 0;
+}
 /**
  * The main function.
  *
@@ -13,21 +24,25 @@ import { Client } from "./index.js";
  */
 async function main(): Promise<number> {
 	let code = 0;
-	try {
-		const client = new Client("https://localhost:6443", {logAlerts: true, raiseErrorAlerts: false});
-		await client.login("admin", "twelve12");
-		const about = await client.about();
-		console.log("about:", about);
-		const sysInf = await client.systemInfo();
-		if (errors(sysInf.alerts ?? []).length > 0) {
-			code = 1;
-		}
-		console.log("system/info:", sysInf);
-	} catch (err) {
-		console.error("API usage failure:", err);
-		code = 2;
-	}
+	const client = new Client("https://localhost:6443", {logAlerts: true, logger: console, raiseErrorAlerts: false});
+	await client.login("admin", "twelve12");
+	const about = await client.about();
+	console.log("about:", about);
+	const sysInf = await client.systemInfo();
+	code += checkAlerts(sysInf);
+	console.log("system/info:", sysInf);
 	return code;
 }
 
-main().then(process.exit);
+/**
+ * Handles any errors thrown by `main`.
+ *
+ * @param e The thrown error.
+ */
+function handleErr(e: unknown): void {
+	const msg = e instanceof Error ? e.message : String(e);
+	console.error("client crashed:", msg);
+	process.exit(255);
+}
+
+main().then(ec=>process.exit(ec)).catch(handleErr);
